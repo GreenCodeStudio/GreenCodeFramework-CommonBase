fb29accde47a5c160ec396ccf05f9a9f8f3dbe81
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-06-23 09:47:34 +0200

    rozwój

--- Controllers/Start.php
+++ Controllers/Start.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace Common\Controllers;
-class Start extends \Core\StandardController{
-    function index(){
-        $this->addView('Common','start');
+
+class Start extends \Core\StandardController {
+
+    function index() {
+        $this->addView('Common', 'start');
     }
-    function demo($a, int $b=5, \Exception $c=null){
-        dump($a,$b);
+
+    function demo($a, int $b = 5, \Exception $c = null) {
+        dump($a, $b);
     }
+
 }
