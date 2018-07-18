40ee376c99891042578858ea4e3dc3445a2eedad
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-07-18 20:37:57 +0200

    podstawy wyświetlanai breadcrumba i tasków

--- Views/template.php
+++ Views/template.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html class="color-green">
     <head>
         <title>TODO supply a title</title>
         <meta charset="UTF-8">
@@ -7,9 +7,16 @@
         <link href="/css/build.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <header></header>
+        <header>
+            <div class="tasks"></div>
+        </header>
         <aside data-views="aside"><?php $this->showViews('aside'); ?></aside>
-        <div class="mainContent" data-views="main"><?php $this->showViews('main'); ?></div>
+        <div class="mainContent">
+            <div class="topBar">
+                <div class="breadcrumb"><?php $this->showBreadcrumb(); ?></div>
+            </div>
+            <div data-views="main"><?php $this->showViews('main'); ?></div>
+        </div>
         <script>
             window.controllerInitInfo = <?=json_encode($this->getInitInfo())?>;
         </script>
