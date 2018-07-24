8765eb1edecf4c2c05832511028317e2a8ff8f53
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-07-24 11:49:29 +0200

    PWA

--- Views/template.php
+++ Views/template.php
@@ -4,7 +4,8 @@
         <title>TODO supply a title</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="/css/build.css" rel="stylesheet" type="text/css"/>
+        <link href="/css/build.css" rel="stylesheet" type="text/css">
+        <link rel="manifest" href="/manifest.json">
     </head>
     <body>
         <header>
