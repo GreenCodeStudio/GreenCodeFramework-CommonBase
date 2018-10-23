b7b7e80fbca5b6ebbcceb9e2ed9b567d1f1c522d
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2018-10-23 12:01:20 +0200

    modal przy braku uprawnien

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,30 @@
 export function modal(text, type = 'info', buttons = [{text: 'ok', value: true}]) {
-    console.log(text);
-    let modalContainer=document.createElement('div');
-    modalContainer.className='modalContainer';
-    document.body.appendChild(modalContainer);
+    new Promise((resolve, reject) => {
+        console.log(text);
+        let modalContainer = document.createElement('div');
+        modalContainer.className = 'modalContainer';
+        document.body.appendChild(modalContainer);
 
-    let modal=document.createElement('div');
-    modal.className='modal';
-    modal.dataset.type=type;
-    modalContainer.appendChild(modal);
+        let modal = document.createElement('div');
+        modal.className = 'modal';
+        modal.dataset.type = type;
+        modalContainer.appendChild(modal);
 
-    let textElem=document.createElement('div');
-    textElem.textContent=text;
-    modal.appendChild(textElem);
+        let textElem = document.createElement('div');
+        textElem.className = 'modal-text';
+        textElem.textContent = text;
+        modal.appendChild(textElem);
+
+        for (let button of buttons) {
+            let buttonElem = document.createElement('div');
+            buttonElem.className = 'modal-button';
+            buttonElem.textContent = button.text;
+            buttonElem.onclick = () => {
+                modalContainer.classList.add('closing');
+                setTimeout(() => modalContainer.remove(), 1000);
+                resolve(button.value);
+            };
+            modal.appendChild(buttonElem);
+        }
+    });
 }
