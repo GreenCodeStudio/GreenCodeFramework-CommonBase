7ef3fa7f3a63e1b51dbf1dc97189c69efaa58800
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-05-28 21:45:36 +0200

    foldery

--- Views/loginTemplate.php
+++ Views/loginTemplate.php
@@ -4,7 +4,7 @@
     <title>Title</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/css/build.css" rel="stylesheet" type="text/css"/>
+    <link href="/dist/styles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <?php $this->showViews('main'); ?>
@@ -13,6 +13,6 @@
     window.DEBUG=<?=json_encode($this->isDebug())?>;
     window.DEBUG=<?=json_encode($this->isDebug())?>;
 </script>
-<script src="/js/main.bundle.js" type="text/javascript"></script>
+<script src="/dist/main.js" type="text/javascript"></script>
 </body>
 </html>
--- Views/template.php
+++ Views/template.php
@@ -4,7 +4,7 @@
     <title><?= htmlspecialchars($this->getTitle()) ?></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/css/build.css" rel="stylesheet" type="text/css">
+    <link href="/dist/styles.css" rel="stylesheet" type="text/css">
     <link rel="manifest" href="/manifest.json">
 </head>
 <body>
@@ -29,6 +29,6 @@
     window.controllerInitInfo = <?=json_encode($this->getInitInfo())?>;
     window.DEBUG =<?=json_encode($this->isDebug())?>;
 </script>
-<script src="/js/main.bundle.js" type="text/javascript"></script>
+<script src="/dist/main.js" type="text/javascript"></script>
 </body>
 </html>
