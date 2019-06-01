4f5edd99a0360087bc02e491519c66efa2a02d1a
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-06-01 09:07:31 +0200

    rozwój

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import {Ajax} from "../../Core/js/ajax";
+import {TableManager} from "../../Core/js/table";
 
 document.querySelectorAll('.logoutMyselfBtn').forEach(b => b.onclick = async () => {
     await Ajax.Authorization.logout();
@@ -20,3 +21,20 @@ const login = {
 login.startToken = login.currentToken;
 
 setInterval(login.check.bind(login), 1000);
+
+TableManager.prototype.calcSize = function () {
+    let rowHeight = 40;
+    let totalHeight = window.innerHeight - this.table.offsetTopFull - 40;
+    this.limit = Math.floor(totalHeight / rowHeight);
+    if (this.limit < 1 || !isFinite((this.limit)))
+        this.limit = 1;
+};
+addEventListener('resize', () => {
+    document.querySelectorAll('.dataTable').forEach(table => {
+        if (!table.datatable) return;
+        let limit = table.datatable.limit;
+        table.datatable.calcSize();
+        if (table.datatable.limit !== limit)
+            table.datatable.refresh();
+    })
+});
