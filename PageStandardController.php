8d4a2e02553558f4ddb1d54e77d919de7a964ec8
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-07-22 22:12:58 +0200

    poprawki

--- PageStandardController.php
+++ PageStandardController.php
@@ -7,7 +7,7 @@ class PageStandardController extends \Core\StandardController
     public function postAction()
     {
         $userData = \Authorization\Authorization::getUserData();
-        $menu = new \core\Menu();
+        $menu = new \Core\Menu();
         $menuData = $menu->readMenu();
         $this->addView('Common', 'aside', ['menu' => $menuData], 'aside');
         require __DIR__.'/../Common/Views/template.php';
