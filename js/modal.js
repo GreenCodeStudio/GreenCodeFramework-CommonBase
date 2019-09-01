26607e36be5d533086861b3161b3a34fdf9b1fe0
Mateusz Krawczyk <mateuszkrawczyk1996@gmail.com>
2019-09-01 17:24:42 +0200

    bugi

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,16 @@
 export function modal(text, type = 'info', buttons = [{text: 'ok', value: true}]) {
     return new Promise((resolve, reject) => {
         console.log(text);
-        let modalContainer=document.body.add('div.modalContainer');
+        let modalContainer=document.body.addChild('div.modalContainer');
 
-        let modal = modalContainer.add('div.modal');
+        let modal = modalContainer.addChild('div.modal');
         modal.dataset.type = type;
 
-        let textElem = modal.add('div.modal-text');
+        let textElem = modal.addChild('div.modal-text');
         textElem.textContent = text;
 
         for (let button of buttons) {
-            let buttonElem = modal.add('button.modal-button');
+            let buttonElem = modal.addChild('button.modal-button');
             buttonElem.textContent = button.text;
             buttonElem.onclick = () => {
                 modalContainer.classList.add('closing');
