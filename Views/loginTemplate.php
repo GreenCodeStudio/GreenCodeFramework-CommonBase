15ab388d3e2a893affc48e7ccd53768d9ee1f50f
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2023-05-12 15:16:01 +0200

    colors

--- Views/loginTemplate.php
+++ Views/loginTemplate.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Title</title>
+    <title><?= htmlspecialchars($this->getTitle()) ?></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/dist/main.css" rel="stylesheet" type="text/css">
